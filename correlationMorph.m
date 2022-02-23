2cb1f7a8dec160b89088c208dbe23ccb9dec2a2a
John Brooke <jmartinbrooke@gmail.com>
2022-02-23 11:46:47 +0000

    Corrected matlab script

--- correlationMorph.m
+++ correlationMorph.m
@@ -68,39 +68,6 @@ print ( './logsMorph/corrMorph1Adj.png','-dpng')
 
 
 
-load ('./logsMorph/correlate3.data');
-load ('./logsMorph/random_correlate3.data');
-figure(7)
-h1 = histogram(random_correlate3,80)
-h1.Normalization = 'probability'
-title('Correlations for Dn = 36.0 Chi = 0 Both morph 2')
-xlabel('correlation of edges')
-ylabel('frequency')
-hold
-h2 = histogram(correlate3,80)
-h2.Normalization = 'probability'
-h2.FaceColor = 'r'
-h2.FaceAlpha = 0.4
-print ( './logsMorph/corrMorph2Both.png','-dpng')
-
-figure(8)
-h1 = histogram(random_correlate3,80)
-h1.Normalization = 'probability'
-title('Correlations for Dn = 36.0 Chi = 0 Rand morph 2')
-xlabel('correlation of edges')
-ylabel('frequency')
-print ( './logsMorph/corrMorph2Rand.png','-dpng')
-
-figure(9)
-h1 = histogram(correlate3,80)
-h1.Normalization = 'probability'
-title('Correlations for Dn = 36.0 Chi = 0 Adj morph 2')
-xlabel('correlation of edges')
-ylabel('frequency')
-h1.FaceColor = 'r'
-print ( './logsMorph/corrMorph2Adj.png','-dpng')
-
-
 
 
 
